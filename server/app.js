7b37b061d6330c32156d612ff751d17ae78f8458
Renan Leonello Castro <renanleonellocastro@gmail.com>
2022-04-16 17:22:12 -0300

    Adicionado tratamento de erro e dependencias morgan e nodemon

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const produtsRoute = require('./routes/products');
 const ordersRoute = require('./routes/orders');
 
+app.use(morgan('dev'));
 app.use('/produtos', produtsRoute);
 app.use('/pedidos', ordersRoute);
+
+// Tratamento de erro
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
