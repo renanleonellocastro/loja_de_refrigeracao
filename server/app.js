65287a9f7bb664cb4aaae5c23c9ec954ae86763c
Renan Leonello Castro <renanleonellocastro@gmail.com>
2022-04-16 17:33:52 -0300

    Adicionado dependencia do body-parser e implementado cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const produtsRoute = require('./routes/products');
 const ordersRoute = require('./routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
+app.use(bodyParser.json()); // json de entrada no body
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 app.use('/produtos', produtsRoute);
 app.use('/pedidos', ordersRoute);
 
