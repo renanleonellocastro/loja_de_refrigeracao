a3a689195c88c4990dfc47f0379701539eeffeae
Renan Leonello Castro <renanleonellocastro@gmail.com>
2022-06-11 23:15:24 -0300

    Atualizado leitura de variavel de ambiente

---
+++ server/config.js
@@ -0,0 +1,46 @@
+class Config
+{
+    constuctor()
+    {
+        this.reload();
+    }
+
+    reload()
+    {
+        this.postgresUser = process.env.DATABASE_URL.split(':')[1].split('//')[1];
+        this.postgresPass = process.env.DATABASE_URL.split(':')[2].split('@')[0];
+        this.postgresHost = process.env.DATABASE_URL.split(':')[2].split('@')[1];
+        this.postgresPort = process.env.DATABASE_URL.split(':')[3].split('/')[0];
+        this.postgresName = process.env.DATABASE_URL.split(':')[3].split('/')[1];
+    }
+
+    getDbUser()
+    {
+        return this.postgresUser;
+    }
+
+    getDbName()
+    {
+        return this.postgresName;
+    }
+
+    getDbHost()
+    {
+        return this.postgresHost;
+    }
+
+    getDbPort()
+    {
+        return this.postgresPort;
+    }
+
+    getDbPassword()
+    {
+        return this.postgresPass;
+    }
+}
+
+const config = new Config();
+config.reload();
+
+module.exports.config = config;
--- server/database/postgres.js
+++ server/database/postgres.js
@@ -1,13 +1,16 @@
 const { reject } = require('bcrypt/promises');
 const postgres = require ('pg');
+const config = require('../config');
+
+const cfg = config.config;
 
 const pool = new postgres.Pool({
-    "user" : process.env.PBUSER,
-    "host" : process.env.PBHOST,
-    "port" : process.env.PGPORT,
-    "database" : process.env.PBDATABASE,
-    "password" : process.env.PGPASSWORD,
-})
+    "user" : cfg.getDbUser(),
+    "host" : cfg.getDbHost(),
+    "port" : cfg.getDbPort(),
+    "database" : cfg.getDbName(),
+    "password" : cfg.getDbPassword(),
+});
 
 exports.execute = (query, params=[]) => {
     return new Promise((resolve,reject) => {
